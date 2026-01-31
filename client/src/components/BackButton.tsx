954f819388627bb84f1028709c4e4a2a9a370e1a
PMabertay <2502326@uad.ac.uk>
2026-01-31 20:10:02 +0000

    Fix: Adjust Back button positioning for mobile (more natural placement)

--- client/src/components/BackButton.tsx
+++ client/src/components/BackButton.tsx
@@ -3,7 +3,7 @@ import { Link } from "wouter";
 export default function BackButton() {
     return (
         <Link href="/">
-            <button className="ptv-sub-button" style={{ position: 'absolute', top: '2rem', left: '2rem', zIndex: 9999 }}>
+            <button className="ptv-sub-button absolute top-4 left-4 md:top-8 md:left-8 z-[9999]">
                 BACK
             </button>
         </Link>
